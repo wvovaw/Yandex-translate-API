5a299d1f7edb8140e5092d4fed19752092cd9d1e
wvovaw <vladimir5313375015@gmail.com>
2019-12-04 02:24:49 +0500

    some refactoring

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Translator
 ## Using Yandex Translate API
+
+### Get your personal  API key [here](https://translate.yandex.com/developers/keys)
+
--- yandextrans.py
+++ yandextrans.py
@@ -2,15 +2,21 @@ from urllib.request import urlopen, Request
 from urllib.parse import quote
 import re
 
-AUTHKEY = 'trnsl.1.1.20191202T210131Z.0efaaf74a79b747b.66acfde631d5a271bca8f55e333f0f42e668fdc7'
+AUTHKEY = 'GIVE IT HERE: https://translate.yandex.com/developers/keys'
 HOST = 'https://translate.yandex.net/api/v1.5/tr/translate'
-lang = 'ru'
-tranlate_to = 'en-ru'
-text_format = 'plain'
 
 
-def translate(text):
-    query = HOST + '?key=' + AUTHKEY + '&text=' + text  + '&lang=' + lang + '&format=' + text_format
+def main():
+    original_text = quote(input())
+    target_lang = 'en'
+    translated_text = translate(original_text, target_lang)
+    print('====[Yandex Translate: to ' + target_lang + ']==================')
+    print(translated_text)
+    print('===============================================')
+
+
+def translate(origin_text, target_lang):
+    query = HOST + '?key=' + AUTHKEY + '&text=' + origin_text  + '&lang=' + target_lang + '&format=plain'
     try:
         request = Request(query)
     except:
@@ -27,19 +33,9 @@ def translate(text):
     match = re.compile('<text>(.*?)</text>').search(str(xml))
     if match:
         translated_text = match.group(1)
-        print('==[Yandex Translate: ' + tranlate_to + ']====================')
-        print(translated_text)
-        print('===============================================')
         return translated_text
     else:
         return None
-
-
-def main():
-    original_text = quote(input())
-    translated_text = translate(original_text)
-    print(translated_text)
-
 
 if __name__ == "__main__":
     main()
